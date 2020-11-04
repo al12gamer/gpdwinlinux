915c1c9cf9214d785b1ca1c887449e56510daa3c
Alex <al12gamer@users.noreply.github.com>
2020-11-03 17:30:21 -0800

    remove gnome theme settings
    dont need it if run as root

--- fedora_winmax.sh
+++ fedora_winmax.sh
@@ -1,35 +1,29 @@
 #!/usr/bin/env bash
 
 # create firmware file for screen and move to grub
-sudo mkdir /lib/firmware/edid
+mkdir /lib/firmware/edid
 base64 --decode ~/gpdwinlinux/winmaxscreen > /lib/firmware/edid/gpdwinmax.bin
 echo 'GRUB_CMDLINE_LINUX="video=eDP-1:800x1280 drm.edid_firmware=eDP-1:edid/gpdwinmax.bin fbcon=rotate:1"' >> /etc/default/grub
-sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
+grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
 echo "GPD Win Max firmware installed"
 sleep .5
-
-# gnome settings
-echo "enabling dark mode in gui"
-sleep .5
-gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 
 # enable rpmfusion
 echo "installing extra repos and gaming packages"
 sleep .5
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -yq
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
-sudo dnf groupupdate core -y
+dnf groupupdate core -y
 
-# install some basic stuff to get gaming
-sudo dnf install steam lutris wine vim neofetch gamemode -y
+# install some basic stuff you would need otherwise
+dnf install steam lutris wine vim neofetch gamemode gnome-tweaks -y
 
 # update repositories
 
-sudo dnf check-update -y
+dnf check-update -y
 
 # upgrade packages
-sudo dnf upgrade -y
-sudo dnf autoremove -y
+dnf upgrade -y
+dnf autoremove -y
 echo "Script complete, please reboot!"
 sleep 1
