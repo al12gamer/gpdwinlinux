7dba3b71b2489c18bd179cc4296ae3ce822567dd
Alex <al12gamer@users.noreply.github.com>
2020-11-04 22:02:47 -0800

    Create ubuntu_winmax.sh

---
+++ ubuntu_winmax.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# create firmware file for screen and move to grub
+mkdir /lib/firmware/edid
+base64 --decode ~/gpdwinlinux/winmaxscreen > /lib/firmware/edid/gpdwinmax.bin
+echo 'GRUB_CMDLINE_LINUX="video=eDP-1:800x1280 drm.edid_firmware=eDP-1:edid/gpdwinmax.bin fbcon=rotate:1"' >> /etc/default/grub
+sudo update-grub
+echo "GPD Win Max screen firmware installed"
+sleep .5
+
+# check drivers and required repos
+echo "Installing extra repos and gaming packages"
+sleep .5
+
+sudo ubuntu-drivers autoinstall -y
+sudo add-apt-repository ppa:lutris-team/lutris -y
+sudo add-apt-repository ppa:libretro/stable -y
+
+# install some basic stuff you would need otherwise
+sudo apt update -y
+sudo apt install steam-installer meson lutris vim neofetch gamemode gnome-tweaks retroarch* -y
+# meson may be needed to build gamemode in the future if it still isn't in main repos
+
+# upgrade packages
+sudo apt update -y && sudo apt upgrade -y
+
+echo "Script complete, please reboot!"
+sleep 1
