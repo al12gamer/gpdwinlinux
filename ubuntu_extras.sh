d56047004deb18ddb5a5dd8ea49b7cb87dbb4000
Alex <al12gamer@users.noreply.github.com>
2020-11-04 22:33:59 -0800

    added retroarch

--- ubuntu_extras.sh
+++ ubuntu_extras.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# add retroarch
+echo "Installing RetroArch"
+sleep .5
+sudo add-apt-repository ppa:libretro/stable
+sudo apt update -y && sudo apt install retroarch* -y
+
 # add protonfixes
-echo "Installing protontricks"
+echo "Installing protonfixes"
 sleep .5
 sudo add-apt-repository ppa:jacotsu/protonfixes
 sudo apt update -y && sudo apt install protonfixes -y
@@ -13,6 +19,7 @@ sudo apt install curl unzip -y
 git clone https://github.com/die-zuckerschnecke/proton-ge-custom-updater.git
 cd ~/proton-ge-custom-updater
 bash ./proton-ge-custom-updater.sh
-
+echo "In the future, please open the proton-ge-custom-updater folder and run the script if you want a newer GloriousEggroll proton version"
+sleep 1
 echo "Script complete, feel free to exit"
 sleep .5
