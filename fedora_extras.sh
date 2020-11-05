86686d31b666bc361a784171b1017317bd6346bc
Alex <al12gamer@users.noreply.github.com>
2020-11-04 22:24:48 -0800

    added messages, update before install PU

--- fedora_extras.sh
+++ fedora_extras.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
 # add flathub and retroarch
+echo "Adding flathub, installing RetroArch"
+sleep .5
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub org.libretro.RetroArch
 
-# add third party software
-
 # add Proton Updater from COPR
+echo "Grabbing the awesome ProtonUpdater package"
+sleep .5
 sudo dnf copr enable david35mm/ProtonUpdater -y
-sudo dnf install ProtonUpdater -y
 sudo dnf update -y
+sudo dnf install ProtonUpdater -y
+
+echo "Script complete! Feel fre to exit."
+sleep .5
