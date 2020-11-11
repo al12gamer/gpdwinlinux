2dff695ce129c71213687d0bd5158a23cca82d8a
Thomas Castleman <contact@draugeros.org>
2020-11-10 20:31:02 -0500

    added Solus support

--- solus_winmax.sh
+++ solus_winmax.sh
@@ -1,17 +1,22 @@
-
-#!/usr/bin/env bash
-
-# create firmware file for screen and move to grub
-mkdir /lib/firmware/edid
-# Solus USES CLR Instead....need fixing base64 --decode ~/gpdwinlinux/winmaxscreen > /lib/firmware/edid/gpdwinmax.bin
-# change for CLR echo 'GRUB_CMDLINE_LINUX="video=eDP-1:800x1280 drm.edid_firmware=eDP-1:edid/gpdwinmax.bin fbcon=rotate:1"' >> etc/kernel/cmdline
-# clr-boot-manager update
-# echo "GPD Win Max firmware installed"
-# sleep .5
+#!/bin/bash
+if [ "$UID" != "0" ]; then
+	echo "ERROR: NOT RUNNING AS ROOT!"
+	echo "Please run this script as root to have it work correctly!"
+	exit 2
+fi
+# create firmware file for screen, as well as folder to hold command line options and move to CBM
+mkdir -p /lib/firmware/edid /etc/kernel/cmdline.d
+# Standard firmware should work just fine
+base64 --decode ~/gpdwinlinux/winmaxscreen > /lib/firmware/edid/gpdwinmax.bin
+echo 'video=eDP-1:800x1280 drm.edid_firmware=eDP-1:edid/gpdwinmax.bin fbcon=rotate:1' > /etc/kernel/cmdline.d/gpdwinlinux.conf
+clr-boot-manager update
+echo "GPD Win Max firmware installed"
+sleep .5
 
 # install gaming packages and nvidia drivers
 echo "installing steam etc..."
 sleep .5
+eopkg update-repo
 eopkg install steam lutris wine vim neofetch gamemode -y
 
 # update repositories
