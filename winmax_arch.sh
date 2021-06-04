ed7fedc911dc1f8a1ba6354a85cd8f5ca5b9e6a8
Alex <al12gamer@users.noreply.github.com>
2021-06-03 19:08:22 -0600

    updated grub command

--- winmax_arch.sh
+++ winmax_arch.sh
@@ -8,16 +8,16 @@ fi
 mkdir /lib/firmware/edid
 base64 --decode ~/gpdwinlinux/winmaxscreen > /lib/firmware/edid/gpdwinmax.bin
 echo 'GRUB_CMDLINE_LINUX="video=eDP-1:800x1280 drm.edid_firmware=eDP-1:edid/gpdwinmax.bin fbcon=rotate:1"' >> /etc/default/grub
-grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
+grub-mkconfig -o /boot/grub/grub.cfg
 echo "GPD Win Max firmware installed"
-sleep .5
+sleep 2
 sudo pacman -Syu
 echo "grabbing steam, lutris, etc"
-sleep 1
+sleep 2
 sudo pacman -S steam lutris python-pip
 pip install protonup
 echo "grabbing heroic launcher, flatpak"
-sudo yay -Syu heroic-games-launcher-bin
+sudo yay -S heroic-games-launcher-bin
 sudo pacman -S flatpak
 echo "all good to go!"
 sleep 2
