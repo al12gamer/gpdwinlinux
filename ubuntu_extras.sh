c48e0992b32c81b16c8df0e35c08490c3e366df1
Alex <al12gamer@users.noreply.github.com>
2021-07-19 09:34:49 -0600

    replace proton-updater with protonup

--- ubuntu_extras.sh
+++ ubuntu_extras.sh
@@ -25,16 +25,10 @@ cd legendary
 python3 setup.py install
 cd
 
-# add proton-ge-updater
-echo "Grabbing script for updating Proton-GE builds easily"
+# add protonup
+echo "Grabbing protonup for updating Proton-GE builds easily"
 sleep .5
-apt install curl unzip -y
-git clone https://github.com/die-zuckerschnecke/proton-ge-custom-updater.git
-cd ~/proton-ge-custom-updater
-# We have to use the username for the user we want to become since we are running as root
-# Best way to do that is reading /etc/passwd and getting the username for UID 1000
-su $(grep '1000' /etc/passwd | sed 's/:/ /g' | awk '{print $1}') -c bash ./proton-ge-custom-updater.sh
-echo "In the future, please open the proton-ge-custom-updater folder and run the script if you want a newer GloriousEggroll proton version"
-sleep 1
+sudo apt install curl python-pip -y
+pip install protonup
 echo "Script complete, feel free to exit"
 sleep .5
