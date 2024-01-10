8555f225e332292eaff695fd8c0d55aa5d39a59d
Alex <al12gamer@users.noreply.github.com>
2024-01-10 13:01:00 -0700

    Update gpdwinlinux.sh
    prep for gpd win mini detection

--- gpdwinlinux.sh
+++ gpdwinlinux.sh
@@ -4,6 +4,8 @@ if [ "$UID" != "0" ]; then
 	echo "Please run this script as root to have it work correctly!"
 	exit 2
 fi
+# Get current hardware, dmidecode
+# board=$(dmidecode -t baseboard)
 # Get current distro, in lowercase
 distro=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
 if [ "$distro" == "ubuntu" ]; then
